2136cc1b86afff59029843f1140b8e477cfa6ac9
Sakar-322 <sakar.droogie@gmail.com>
2024-07-26 13:20:50 +0545

    pnpm and sheet

--- src/components/FilterSheet.tsx
+++ src/components/FilterSheet.tsx
@@ -5,6 +5,8 @@ import * as React from "react"
 import { Filter } from "lucide-react"
 import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
+import { Slider } from "./ui/slider"
+import { Input } from "./ui/input"
 
 
 
@@ -57,7 +59,42 @@ const FilterSheet: React.FC = () => {
         {/* Budget Range */}
         <div className="my-4">
           <h3 className="font-semibold">Budget Range</h3>
-          {/* Add your Budget Range filter components here */}
+          <div className="flex items-center space-x-2">
+            <Input
+              type="number"
+              value={filterValues.budgetRange[0]}
+              onChange={(e) => handleFilterChange('budgetRange', [Number(e.target.value), filterValues.budgetRange[1]])}
+              className="w-24"
+              min={100}
+              max={1000}
+            />
+            <span>-</span>
+            <Input
+              type="number"
+              value={filterValues.budgetRange[1]}
+              onChange={(e) => handleFilterChange('budgetRange', [filterValues.budgetRange[0], Number(e.target.value)])}
+              className="w-24"
+              min={100}
+              max={1000}
+            />
+          </div>
+          {/* <Slider
+            min={100}
+            max={1000}
+            value={filterValues.budgetRange}
+            onValueChange={(value) => handleFilterChange('budgetRange', value as [number, number])}
+            className="mt-4"
+          >
+            <Slider.Track className="relative bg-gray-200 h-2 rounded-full">
+              <Slider.Range className="absolute bg-primary h-2 rounded-full" />
+            </Slider.Track>
+            <Slider.Thumb className="block w-4 h-4 bg-white border border-primary rounded-full shadow" />
+            <Slider.Thumb className="block w-4 h-4 bg-white border border-primary rounded-full shadow" />
+          </Slider> */}
+          <div className="flex justify-between mt-2">
+            <div className="tooltip">{`$${filterValues.budgetRange[0]}`}</div>
+            <div className="tooltip">{`$${filterValues.budgetRange[1]}`}</div>
+          </div>
         </div>
         
         {/* Room and Beds */}
